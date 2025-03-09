9a831a327d613be8055b5fdbabea0931fa1a83af
Nitin Pratap Singh <118966133+NitinSingh07@users.noreply.github.com>
2025-03-09 13:59:58 +0530

    Remove commented-out imports in providers.tsx for cleaner code

--- client/src/app/providers.tsx
+++ client/src/app/providers.tsx
@@ -3,8 +3,6 @@
 import StoreProvider from "@/state/redux";
 import { Authenticator } from "@aws-amplify/ui-react";
 import Auth from "./(auth)/authProvider";
-// import { Authenticator } from "@aws-amplify/ui-react";
-// import Auth from "./(auth)/authProvider";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
